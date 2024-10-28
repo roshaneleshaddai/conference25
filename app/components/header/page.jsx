fa8167ac6837c285d473406a4b6548e57374f017
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-10-28 17:03:55 +0530

    Update page.jsx
    -- ng

--- app/components/header/page.jsx
+++ app/components/header/page.jsx
@@ -54,7 +54,7 @@ const Header = () => {
     >
       <source src="/images/bg.mp4" type="video/mp4" />
     </video> */}
-      <div className="flex items-center justify-around lg:p-2 p-0">
+      <div className="flex items-center justify-around">
         {/* Logo Section */}
         {/* <div className="flex items-center justify-around space-x-0 lg:space-x-4"> */}
           <Image
