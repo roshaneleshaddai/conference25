1d5a442a54b575fde928c69888c6f894ee4df72a
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-19 16:40:04 +0530

    Update page.jsx
    md to lg
    --ng

--- app/contact_us/page.jsx
+++ app/contact_us/page.jsx
@@ -17,20 +17,20 @@ const contact = () => {
   ]
   return (
     <>
-    <div className="mt-44 md:mt-96"></div>
+    <div className="mt-44 lg:mt-96"></div>
 
     <Imageplacer name="Contact Us"/>
-    <div className='flex felx-col md:flex-row justify-center gap-10 m-32 '>
+    <div className='flex felx-col lg:flex-row justify-center gap-10 m-32 '>
       <div>
-         <h1 className='text-2xl md:text-5xl font-bold pb-5'>Our Location</h1>
-         <p className='text-lg md:text-2xl '>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
+         <h1 className='text-2xl lg:text-5xl font-bold pb-5'>Our Location</h1>
+         <p className='text-lg lg:text-2xl '>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
           Chalasani Nagar,<br />
           Kanuru,<br />
           Vijayawada, Andhra Pradesh 520007.</p>
       </div>
       <div>
-      <h1 className='text-2xl md:text-5xl font-bold pb-5'>Drop Us a Line</h1>
-         <p className='text-lg md:text-2xl '>
+      <h1 className='text-2xl lg:text-5xl font-bold pb-5'>Drop Us a Line</h1>
+         <p className='text-lg lg:text-2xl '>
           {contact1.map((c1)=>{
            return( <li key={c1.id} className="flex items-center"><IoCallOutline className='mr-3' />{c1.name} - {c1.ph}</li>
          ) })}
