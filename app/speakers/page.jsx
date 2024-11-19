649d12f736244652c8d2a7024f30cff2a2aa6f00
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-19 17:06:58 +0530

    Update page.jsx
    md to lg

--- app/speakers/page.jsx
+++ app/speakers/page.jsx
@@ -3,7 +3,7 @@ import Imageplacer from '../components/imageplacer/page'
 const speakers = () => {
   return (
     <>
-    <div className="mt-44 md:mt-96"></div>
+    <div className="mt-44 lg:mt-96"></div>
 
     <Imageplacer name="Guests & Speakers"/>
     </>
