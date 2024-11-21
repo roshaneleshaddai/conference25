4a68cbeb9b44ed3e610a5144ec26d5f7a28accb3
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-21 20:43:05 +0530

    Update page.jsx
    text size adjusted
    --ng

--- app/important_dates/page.jsx
+++ app/important_dates/page.jsx
@@ -11,7 +11,7 @@ const ImportantDates = () => {
       <h1 className="text-2xl lg:text-5xl md:mt-96 lg:pt-10 pt-20 mt-44 lg:mb-0 mb-4 font-extrabold  text-center text-black">Important Dates </h1>
       {/* <Dates /> */}
       <div className='p-10 lg:m-10 m-4 flex flex-col items-center'>
-              <ul className="text-sm md:text-2xl space-y-4">
+              <ul className="text-base md:text-2xl space-y-4">
                 <li className="flex items-center">
                   <FaRegCalendarAlt className="mr-2" />
                   <span>2nd June 2025 - Deadline for Submission</span>
