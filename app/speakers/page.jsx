2cd05344d5e2211feac2d89a30f87a9dca71da7d
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-21 18:59:26 +0530

    Update page.jsx
    image placer-
    --ng

--- app/speakers/page.jsx
+++ app/speakers/page.jsx
@@ -5,7 +5,8 @@ const speakers = () => {
     <>
     <div className="mt-44 lg:mt-92"></div>
 
-    <Imageplacer name="Guests & Speakers"/>
+    {/* <Imageplacer name="Guests & Speakers"/> */}
+    <h1 className="text-2xl lg:text-5xl md:mt-96 lg:pt-10 pt-20 mt-44 lg:mb-0 mb-4 font-extrabold  text-center text-black">Guests & Speakers</h1>
     </>
   )
 }
