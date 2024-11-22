656580ae50e7b980fc74c6e4a1748bbd8af8ebbc
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-22 16:26:18 +0530

    Update page.jsx
    alignment adjusted
    --ng

--- app/contact_us/page.jsx
+++ app/contact_us/page.jsx
@@ -31,16 +31,16 @@ const contact = () => {
 
     {/* <Imageplacer name="Contact Us"/> */}
     <h1 className="text-2xl lg:text-5xl md:mt-96 lg:pt-10 pt-20 mt-44 lg:mb-0 mb-4 font-extrabold  text-center text-black">Contact Us</h1>
-    <div className='flex flex-col lg:flex-row justify-center gap-10 lg:m-32 my-20 lg:mx-16 mx-10'>
+    <div className='flex flex-col lg:flex-row justify-evenly items-center gap-10 lg:m-32 my-20 lg:mx-16 mx-10'>
       <div>
-         <h1 className='text-2xl lg:text-5xl font-bold pb-5'>Our Location</h1>
-         <p className='text-lg lg:text-2xl '>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
-          Chalasani Nagar,<br />
+         <h1 className='text-2xl lg:text-5xl font-bold pb-5 text-center'>Our Location</h1>
+         <p className='text-lg lg:text-2xl text-center'>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
+          Chalasani Nagar,
           Kanuru,<br />
           Vijayawada, Andhra Pradesh 520007.</p>
       </div>
       <div>
-      <h1 className='text-2xl lg:text-5xl font-bold pb-5'>Drop Us a Line</h1>
+      <h1 className='text-2xl lg:text-5xl font-bold pb-5 text-center'>Drop Us a Line</h1>
          <p className='text-base lg:text-2xl '>
           {contact1.map((c1)=>{
            return( <li key={c1.id} className="flex items-center"><IoCallOutline className='mr-3' />{c1.name} - {c1.ph}</li>
