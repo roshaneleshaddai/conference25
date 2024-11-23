758a9b52d24d774c7ab19385aaf47bc1d1f91a3d
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-23 18:10:02 +0530

    Update page.jsx

--- app/components/dates/page.jsx
+++ app/components/dates/page.jsx
@@ -11,15 +11,15 @@ const Dates = () => {
           <ul className="text-sm md:text-2xl  space-y-4">
              <li className="flex items-center">
                <FaRegCalendarAlt className="mr-2" />
-               <span>2nd June 2025 - Deadline for Submission </span>
+               <span>1st May 2025 - Deadline for Submission </span>
              </li>
              <li className="flex items-center">
                <FaRegCalendarAlt className="mr-2" />
-               <span>25th June 2025 - Acceptance Notification & Registrations</span>
+               <span>1st June 2025 - Acceptance Notification & Registrations</span>
              </li>
              <li className="flex items-center">
                <FaRegCalendarAlt className="mr-2" />
-               <span>30th June 2025 -  Registration & Final Paper Submission</span>
+               <span>15th June 2025 -  Registration & Final Paper Submission</span>
              </li>
            </ul>
 
