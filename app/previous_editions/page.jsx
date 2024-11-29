4754ba2badfc3eec09f57d74e62f10b721293a62
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-29 20:24:10 +0530

    Update page.jsx

--- app/previous_editions/page.jsx
+++ app/previous_editions/page.jsx
@@ -11,7 +11,7 @@ export default function Editions_News (){
         <div className="mt-48 lg:mt-92 lg:p-32 mb-0 flex justify-center">
       <section id="editions" className="bg-white lg:w-1/2 shadow-xl border-2 border-black/10 rounded-tr-[4rem] rounded-bl-[4rem] md:px-20 md:py-10 md:m-20 px-4 py-5 m-8 w-full">
         <div className="flex justify-center">
-          <div className="flex flex-col lg:mx-12 mx-8">
+          <div className="flex flex-col lg:mx-6 mx-8">
             <div className="font-bold text-2xl lg:text-3xl pb-4">
             Previous Editions
             <hr className="h-1 my-4 bg-[#87A2FF] border-0 " />
